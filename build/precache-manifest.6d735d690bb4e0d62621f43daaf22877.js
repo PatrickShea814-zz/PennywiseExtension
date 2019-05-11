b7331b498e1e195a3e0dd437c7c195191e4b6b5a
Patrick Shea <patrickshea814@gmail.com>
2019-05-11 18:09:43 -0400

    add build

---
+++ build/precache-manifest.6d735d690bb4e0d62621f43daaf22877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a35ab01c2b8f132e06ea09de0fbc57b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d882e9b2218aab7a2760",
+    "url": "/static/css/main.c8cde240.chunk.css"
+  },
+  {
+    "revision": "a1470695e69ae03ede6b",
+    "url": "/static/js/2.ce270ede.chunk.js"
+  },
+  {
+    "revision": "d882e9b2218aab7a2760",
+    "url": "/static/js/main.c203636f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d4affa1085e7f83c51ad0e0bf74ac9d",
+    "url": "/static/media/colorfullpw.1d4affa1.png"
+  }
+]);
